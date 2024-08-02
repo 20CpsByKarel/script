c78ab45c5b1c6e61094887ea98f0791e58293758
20CpsByKarel <87567412+20CpsByKarel@users.noreply.github.com>
2024-08-02 16:52:43 +0200

    Update locate.js

--- locate.js
+++ locate.js
@@ -1,77 +1,79 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const key = 'ipified';
-    const val = 'true';
-    const ses = sessionStorage.getItem(key);
-    console.log('SESH: ' + ses);
+   const key = 'ipified';
+const val = 'true';
+const ses = sessionStorage.getItem(key);
+console.log('SESH: ' + ses);
 
-    if (!ses) {
-        console.log("Running ipify");
+if (!ses) {
+    console.log("Running ipify");
 
-        fetch("https://api.ipify.org?format=json")
-            .then(response => response.json())
-            .then(data => {
-                var ipAddress = data.ip;
-                console.log('IP Address: ' + ipAddress);
+    fetch("https://api.ipify.org?format=json")
+        .then(response => response.json())
+        .then(data => {
+            var ipAddress = data.ip;
+            console.log('IP Address: ' + ipAddress);
 
-                fetch("https://ipinfo.io/" + ipAddress + "/json")
-                    .then(response => response.json())
-                    .then(data => {
-                        var country = data.country;
-                        console.log("Země: " + country);
+            fetch("https://ipinfo.io/" + ipAddress + "/json")
+                .then(response => response.json())
+                .then(data => {
+                    var country = data.country;
+                    console.log("Země: " + country);
 
-                        var countryCodes = {
-                            CZ: "cz",
-                            EN: "en"
-                        };
+                    var countryCodes = {
+                        CZ: "cz",
+                        EN: "en"
+                    };
 
-                         var countryCode = countryCodes[country] || "en";
-                        if (!countryCode) {
+                    var countryCode = countryCodes[country] || "en";
+                    if (!countryCode) {
+                        countryCode = "en";
+                    }
+
+                    if (countryCode === "cz") {
+                        const userLang = navigator.language || navigator.userLanguage;
+                        console.log('User Language: ' + userLang);
+                        if (userLang.startsWith("cs") || userLang.startsWith("sk")) {
+                            countryCode = "cz";
+                        } else {
                             countryCode = "en";
                         }
+                    } else {
+                        countryCode = "tl";
+                    }
 
-                        if (countryCode === "cz") {
-                            const userLang = navigator.language || navigator.userLanguage;
-                            console.log('User Language: ' + userLang);
-                            if (userLang.startsWith("cs") || userLang.startsWith("sk")) {
-                                countryCode = "cz";
+                    console.log('Country Code: ' + countryCode);
+
+                    // Otevře rozbalovací menu
+                    const toggleElement = document.querySelector('.navbar-toggler.dropdown-toggle');
+                    if (toggleElement) {
+                        toggleElement.click();
+
+                        // Zpoždění pro zajištění načtení rozbalovacího menu
+                        setTimeout(() => {
+                            const dropdownMenu = document.querySelector('.dropdown-menu.dropdown-primary.langs-dropdown-left.show');
+                            const aElement = dropdownMenu ? dropdownMenu.querySelector('a.flag-' + countryCode) : null;
+                            console.log('aElement: ', aElement);
+
+                            if (aElement) {
+                                aElement.click();
+                                sessionStorage.setItem(key, val);
                             } else {
-                                countryCode = "tl";
+                                console.log("Country code not found in dropdown menu");
                             }
-                        } else {
-                            countryCode = "en";
-                        }
+                        }, 500); // 500 ms zpoždění
+                    } else {
+                        console.log("Toggle element not found");
+                    }
+                })
+                .catch(() => {
+                    console.log("CHYBA při získávání země");
+                });
+        })
+        .catch(() => {
+            console.log("CHYBA při získávání IP adresy");
+        });
+} else {
+    console.log("Skipping language auto-selection: already successfully run or not Czech page");
+}
 
-                        console.log('Country Code: ' + countryCode);
-
-                        // Otevře rozbalovací menu
-                        const toggleElement = document.querySelector('.navbar-toggler.dropdown-toggle');
-                        if (toggleElement) {
-                            toggleElement.click();
-
-                            // Zpoždění pro zajištění načtení rozbalovacího menu
-                            setTimeout(() => {
-                                const aElement = document.querySelector('a.flag-' + countryCode);
-                                console.log('aElement: ', aElement);
-
-                                if (aElement) {
-                                    aElement.click();
-                                    sessionStorage.setItem(key, val);
-                                } else {
-                                    console.log("Country code not found in dropdown menu");
-                                }
-                            }, 500); // 500 ms zpoždění
-                        } else {
-                            console.log("Toggle element not found");
-                        }
-                    })
-                    .catch(() => {
-                        console.log("CHYBA při získávání země");
-                    });
-            })
-            .catch(() => {
-                console.log("CHYBA při získávání IP adresy");
-            });
-    } else {
-        console.log("Skipping language auto-selection: already successfully run or not Czech page");
-    }
 });
